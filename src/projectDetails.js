2ac2132620c930362b1556dec53533f353b5d2e6
Pratheek Shetty <pratheeka@active.agency>
2022-02-18 15:50:49 +0530

    first commit

---
+++ src/projectDetails.js
@@ -0,0 +1,58 @@
+export const projectDetails = [
+	{
+		name: "Shadow-Caster",
+		content:
+			"Shadow-Caster® has formed relationships with major marine electronic manufacturers to provide integrated solutions utilizing their lighting control technology.I have Developed control panel of this device using React.",
+		image: require("./Assets/shadow-caster.jpg"),
+		link: "https://www.shadow-caster.com/",
+	},
+	{
+		name: "Charitre",
+		content:
+			"Charitre is the news Website developed for a client,  developed both the Front-end and Back-end as per the client Requirement",
+		image: require("./Assets/charitre.jpg"),
+		link: "http://www.charitre.com/",
+	},
+	{
+		name: "Raagiday",
+		content:
+			"This is a business Website developed for a client who owns Biscuit Company. showcasing the the variety of prodcuts manufactured by Ragiday",
+		image: require("./Assets/raagiday.jpg"),
+		link: "https://www.raagiday.com/",
+	},
+	{
+		name: "Repfabric",
+		content:
+			"Repfabric is a admin dashboard developed for a private-sector, which shows the  details or orders and sales",
+		image: require("./Assets/repfab.jpg"),
+		link: "https://repfabric.com/",
+	},
+	{
+		name: "Bodycoat",
+		content:
+			"Bodycoat is company which provides car services in malls and large cities, developed service booking site for them",
+		image: require("./Assets/bodycoat.jpg"),
+		link: "https://bodycoat.in/",
+	},
+	{
+		name: "Ekbazaar",
+		content:
+			"Ekbazaar is a global project which has a section of Trade and Tender, as name indicates user can sell and buy the product",
+		image: require("./Assets/ekbazaar.jpg"),
+		link: "https://trade.ekbazaar.com/",
+	},
+	{
+		name: "Teju-Masala",
+		content:
+			"Teju Masala is food production company based in karnataka , which produces ready made mixes across the country",
+		image: require("./Assets/teju.jpg"),
+		link: "https://tejumasala.com/",
+	},
+	{
+		name: "Parama-Interiors",
+		content:
+			"Parama Interiors is the karnataka based company which offers quality service for its customers, we designed and developed this website according to ther requirement",
+		image: require("./Assets/param.jpg"),
+		link: "http://paramainteriors.com/index.html",
+	},
+];
