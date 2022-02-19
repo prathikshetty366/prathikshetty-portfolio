a02dc9f25d767652af98356dfa36cee478508b96
Pratheek Shetty <pratheeka@active.agency>
2022-02-19 12:20:18 +0530

    integrated mail

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -47,18 +47,7 @@ function Home() {
 					</a>
 				</div>
 				<div class="hero-img">
-					{/* <img src={banner} alt="" /> */}
-					<iframe
-						src="https://giphy.com/embed/L1R1tvI9svkIWwpVYr"
-						width="480"
-						height="270"
-						frameBorder="0"
-						class="giphy-embed"
-						allowFullScreen
-					></iframe>
-					<p>
-						<a href="https://giphy.com/gifs/Pluralsight-computer-technology-coding-L1R1tvI9svkIWwpVYr"></a>
-					</p>
+					<img src={banner} alt="" />
 				</div>
 			</section>
 			{/* <!-- End Hero Section  --> */}
